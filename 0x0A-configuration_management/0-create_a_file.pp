4fba51d4501fb363ce6319a7939a95d544334147
Bereket Kurkura <105045286+bereketkurkura@users.noreply.github.com>
2022-12-09 17:59:42 +0300

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# this manifest will create file tmp/school
-file {'/tmp/school':
-  path    =>'/tmp/school',
+# creates a file in /tmp
+
+file { '/tmp/school':
   content =>'I love Puppet',
-  owner   =>'www-data',
-  group   =>'www-data',
-  mode    =>'0744',
-      }
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
